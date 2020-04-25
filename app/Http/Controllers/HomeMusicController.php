225df78d80c057c77a667b115cf8098e501c4e30
Junior28 <ejuniormartinez@uniguajira.edu.co>
2020-04-24 23:06:42 -0500

    Agregar los modelos

---
+++ app/Http/Controllers/HomeMusicController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\HomeMusic;
+use Illuminate\Http\Request;
+
+class HomeMusicController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\HomeMusic  $homeMusic
+     * @return \Illuminate\Http\Response
+     */
+    public function show(HomeMusic $homeMusic)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\HomeMusic  $homeMusic
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(HomeMusic $homeMusic)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\HomeMusic  $homeMusic
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, HomeMusic $homeMusic)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\HomeMusic  $homeMusic
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(HomeMusic $homeMusic)
+    {
+        //
+    }
+}
---
+++ database/factories/HomeMusicFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\HomeMusic;
+use Faker\Generator as Faker;
+
+$factory->define(HomeMusic::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
